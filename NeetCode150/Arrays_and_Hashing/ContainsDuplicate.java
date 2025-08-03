4df8ef24ca780e1b386a1bc5374eaf5fc4fb6f6b
Abhimanyu <abhimanyuyadav282002@gmail.com>
2025-08-03 09:34:59 -0700

    Add Contains Duplicate solution

---
+++ NeetCode150/Arrays_and_Hashing/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet <Integer> map= new HashSet<>();
+
+        for(int n:nums){
+            if(map.contains(n)){
+                return true;
+            }
+            map.add(n);
+        }
+        return false;
+        
+    }
+}
